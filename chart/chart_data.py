a6334bf842b734cd2493118f91daf2876a8ef1e2
erfields <ericfields09@gmail.com>
2019-07-12 16:11:00 -0700

    Refactored chart data creation workflow to be more straightforward

--- chart/chart_data.py
+++ chart/chart_data.py
@@ -1,75 +1,78 @@
 from datetime import datetime, timedelta
 import pandas as pd
+from collections import OrderedDict
+
+import logging
+logger = logging.getLogger('stockbot')
 
 class ChartData():
+
     def __init__(self, portfolio):
         self.portfolio = portfolio
 
     def load(self, quotes, historicals, start_time, end_time):
-        # Get current price quote concurrently in a separate thread to save time
-        self.current_price = self.__portfolio_current_value(quotes)
-
-        reference_price, historical_price_map = self.__portfolio_historical_values(historicals, start_time, end_time)
-
-        self.series = pd.Series(historical_price_map)
-
         self.name = self.portfolio.name
 
-        self.reference_price = reference_price
-        self.updated_at = datetime.now()
-
-    def __portfolio_current_value(self, quotes):
-        current_value = self.portfolio.cash
+        # Remove any assets without historical data, i.e. missing or delisted assets
+        assets = []
         for asset in self.portfolio.assets():
             instrument_id = str(asset.instrument_id)
-            if instrument_id not in quotes:
-                print("Warning: no quote data exists for stock/option '{}'".format(asset.identifier))
-                continue
-            current_value += quotes[instrument_id].price() * asset.count * asset.unit_count()
+            if instrument_id in historicals:
+                assets.append(asset)
+            elif instrument_id in quotes:
+                logger.info("'{}' has likely been bought out or acquired. Treating as cash.".format(asset.identifier))
+                self.portfolio.cash += quotes[instrument_id].price() * asset.count * asset.unit_count()
+            else:
+                logger.warning("No data exists for stock/option '{}'".format(asset.identifier))
+
+        # Get current price quote concurrently in a separate thread to save time
+        self.current_price = self.__get_portfolio_current_value(assets, quotes)
+
+        self.reference_price = self.__get_reference_price(assets, historicals)
+
+        chart_price_map = self.__get_chart_price_map(assets, historicals, start_time, end_time)
+        self.series = pd.Series(chart_price_map)
+
+        self.updated_at = datetime.now()
+
+    def __get_portfolio_current_value(self, assets, quotes):
+        current_value = self.portfolio.cash
+        for asset in assets:
+            asset_quote = quotes[str(asset.instrument_id)]
+            current_value += asset_quote.price() * asset.count * asset.unit_count()
 
         return current_value
 
-    def __portfolio_historical_values(self, historicals, start_time, end_time):
+    def __get_reference_price(self, assets, historicals):
         reference_price = self.portfolio.cash
-        historical_price_map = {}
 
-        for asset in self.portfolio.assets():
-            instrument_id = str(asset.instrument_id)
-            if instrument_id not in historicals:
-                print("Warning: no historical data exists for stock/option '{}'".format(asset.identifier))
-                continue
+        for asset in assets:
+            asset_historicals = historicals[str(asset.instrument_id)]
+            if asset.type == asset.__class__.STOCK and asset_historicals.previous_close_price:
+                asset_reference_price = asset_historicals.previous_close_price
+            else:
+                asset_reference_price = 0
+                # Use the first non-zero price value
+                for h in asset_historicals.items:
+                    if h.open_price > 0:
+                        asset_reference_price = h.open_price
+                    elif h.close_price > 0:
+                        asset_reference_price = h.close_price
+                    if asset_reference_price:
+                        break
 
-            asset_historical_data = historicals[instrument_id]
-            asset_reference_price, asset_historical_items = self.__process_historicals(asset, asset_historical_data, start_time, end_time)
-            reference_price += asset_reference_price * asset.count
-            for h in asset_historical_items:
-                if h.begins_at not in historical_price_map:
-                    historical_price_map[h.begins_at] = self.portfolio.cash
-                historical_price_map[h.begins_at] += h.close_price * asset.count * asset.unit_count()
+            reference_price += asset_reference_price * asset.count * asset.unit_count()
 
-        return reference_price, historical_price_map
 
-    def __process_historicals(self, asset, historicals, start_time, end_time):
-        # Filter values outside our date ranges
-        while historicals.items and historicals.items[0].begins_at < start_time:
-            historicals.items.pop(0)
-        while historicals.items and historicals.items[-1].begins_at > end_time:
-            historicals.items.pop()
+        return reference_price
 
-        reference_price = None
-        if asset.type == asset.__class__.STOCK and historicals.previous_close_price:
-            reference_price = historicals.previous_close_price
-        else:
-            # Use the first non-zero price value
-            for h in historicals.items:
-                if h.open_price > 0:
-                    reference_price = h.open_price
-                elif h.close_price > 0:
-                    reference_price = h.close_price
-                if reference_price:
-                    break
-            if not reference_price:
-                reference_price = 0
-
-        reference_price *= asset.unit_count()
-        return reference_price, historicals.items
+    def __get_chart_price_map(self, assets, historicals, start_time, end_time):
+        chart_price_map = {}
+        for asset in assets:
+            asset_historicals = historicals[str(asset.instrument_id)]
+            for h in asset_historicals.items:
+                if start_time <= h.begins_at <= end_time:
+                    if h.begins_at not in chart_price_map:
+                        chart_price_map[h.begins_at] = self.portfolio.cash
+                    chart_price_map[h.begins_at] += h.close_price * asset.count * asset.unit_count()
+        return chart_price_map
