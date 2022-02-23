766b8d97a6b0d5e4c8bdc8a63de396326e13afde
Eric Fields <ericfields09@gmail.com>
2022-02-23 15:38:53 -0800

    Remove old credentials file and re-comment database configuration section

--- StockBot/custom_settings.py
+++ StockBot/custom_settings.py
@@ -1,11 +1,11 @@
 # Define a database here. Note that a database is required for the Index feature.
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stockbot',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'stockbot',
+#     }
+# }
 #
 # CACHES = {
 #     'default': {
